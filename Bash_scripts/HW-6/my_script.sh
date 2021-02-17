9e790132647661dcc3c088efd337661ac15147a4
Sergey Ewdokimow <78340773+Engelko@users.noreply.github.com>
2021-02-17 13:39:50 +0300

    Update my_script.sh

--- Bash_scripts/HW-6/my_script.sh
+++ Bash_scripts/HW-6/my_script.sh
@@ -1,59 +1,43 @@
 #!/bin/bash
 TIME=$(date "+%Y.%m.%d-%H.%M.%S")
 IFS=$'\n'
-
-echo "$TIME DIRECTORIES OLDER THAN 10 DAYS"
-for finddir in $(find $HOME -mindepth 1 -maxdepth 1 -type d -ctime +10 | xargs ls -ld |  awk '{if ($1>0) print $1, $6, $7, $8, $9}' )
-do
-echo -e "\033[31m $finddir"
-done
-tput setaf 7
-
-echo "$TIME FILES OLDER THAN 10 DAYS"
-for findfile in $(find $HOME -type f -ctime +10 | xargs ls -la |  awk '{print $1, $6, $7, $8, $9}' )
-do
-echo -e "\033[32m $findfile"
-done
-tput setaf 7
-
-echo "$TIME SYSTEM INFO FOR /"
-PID=$(echo "Total PIDS: `top -bn1 | grep Tasks | awk '{printf $2}'`")
-MEMORY=$(echo "Total MEMORY usage: `free -m | awk 'NR==2{printf "%s/%s", $3,$2 }'` MB")
-CPU=$(echo "Total CPU usage: `top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}'` %")
-HDD=$( echo "Total DISC usage: `df -h | awk '$NF=="/"{printf "%d/%d (%s)\n", $3,$2,$5}'` GB")
-echo -e "\033[33m$PID"
-echo -e "\033[34m$MEMORY"
-echo -e "\033[35m$CPU"
-echo -e "\033[36m$HDD"
-tput setaf 7
-
-echo "$TIME HOST FILE ADD"
 ETC_HOSTS="/etc/hosts"
-if ! sudo grep -qxF '192.168.3.1 myapp.com' $ETC_HOSTS
-then
- sudo echo "192.168.3.1 myapp.com" >> $ETC_HOSTS
- echo -e "\033[35mADD ADDRESS MYAPP.COM TO FILE HOSTS"
-else
- echo -e "\033[32mFILE HOSTS IS COMPLETE!"
-fi
-tput setaf 7
+. ./lib_for_sh.sh
 
-echo "$TIME CHANGE IP FOR MYAPP.COM"
-read -p "Change ip add? 1=yes or 2=no: " change
-if (($change==1))
-then
- read -p "Input ip address for myapp.com: " ip
- sudo sed -i -- "s/.*myapp\.com/$ip myapp.com/g" $ETC_HOSTS
- echo "$ip is a new ip address for myapp.com in hosts file"
-else
- echo "FILE HOSTS IS COMPLETE!"
-fi
+echo "Please touch number:
+1)Show directory older than 10 days
+2)Show files older than 10 days
+3)Show system info
+4)Add host in hosts files
+5)Change ip in hosts files
+6)Find DELETE_ME "
 
-echo "$TIME FIND -d && -f DELETE_ME"
-DEL_FILE_PATH=DELETE_ME
-while [ ! -d  $DEL_FILE_PATH ] && [ ! -f  $DEL_FILE_PATH ]
-do
-sleep 2
-done
-sudo echo "$TIME Directory DELETE_ME is detected!!" >> script.log
-$(date > ~/temp) ; echo "fin"
+read NUM
+
+case $NUM in 
+	1)	
+		echo "$TIME DIRECTORIES OLDER THAN 10 DAYS"
+		DIR_OLDER
+		;;
+
+	2)
+		echo "$TIME FILES OLDER THAN 10 DAYS"
+		FILES_OLDER
+		;;
+	3)
+		echo "$TIME SYSTEM INFO FOR /"
+		SYS_INFO
+		;;
+	4) 	
+		echo "$TIME HOST FILE ADD"
+		ADD_HOSTS
+		;;
+	5)	
+		echo "$TIME CHANGE IP FOR MYAPP.COM"
+		CHANGE_IP
+		;;
+	6)	
+		echo "$TIME FIND -d && -f DELETE_ME"
+		FIND_DELME
+		;;
+esac
