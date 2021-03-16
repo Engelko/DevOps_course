d12bed2d9af3f3bb82f3b1a2bd5626904e47c893
Sergey Ewdokimow <ewdokimowengel@gmail.com>
2021-03-16 20:15:22 +0000

    add number of letters and uppercase

--- Bash_scripts/HW13/script_hw13.sh
+++ Bash_scripts/HW13/script_hw13.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-        echo "Please enter a number A "
-read NUM_A
+        
+declare -i one=1
+declare -i two=2
+declare -i three=3
+declare -i four=4
+declare -i five=5
+	
+	
+read -p "Please enter a number A	" A       
+read -p "Please enter a number B	" B    
+read -p "Please enter a number C	" C
 
-        echo "Please enter a number B "
-read NUM_B
-
-        echo "Please enter a number C "
-read NUM_C
+NUM_A=${A,,}
+NUM_B=${B,,}
+NUM_C=${C,,}
 
 if [[ "$NUM_A" == "$NUM_B" ]] && [[ "$NUM_A" == "$NUM_C" ]]; then
         echo "A equal B equal C";
@@ -20,21 +27,21 @@ elif [[ "$NUM_A" == "$NUM_B" ]] && [[ "$NUM_B" != "$NUM_C" ]]; then
 elif [[ "$NUM_B" == "$NUM_C" ]] && [[ "$NUM_A" != "$NUM_C" ]]; then
         echo "B equal C and not equal A";
 
-elif [[ "$NUM_B" > "$NUM_A" ]] && [[ "$NUM_B" > "$NUM_C" ]] && [[ "$NUM_A" < "$NUM_C" ]]; then
+elif [[ "$NUM_B" -gt "$NUM_A" ]] && [[ "$NUM_B" -gt "$NUM_C" ]] && [[ "$NUM_A" -lt "$NUM_C" ]]; then
         echo "B is greater than A, B is a greater than C , A less than C";
 
-elif [[ "$NUM_B" > "$NUM_A" ]] && [[ "$NUM_B" > "$NUM_C" ]] && [[ "$NUM_A" > "$NUM_C" ]]; then
+elif [[ "$NUM_B" -gt "$NUM_A" ]] && [[ "$NUM_B" -gt "$NUM_C" ]] && [[ "$NUM_A" -gt "$NUM_C" ]]; then
         echo "B is greater than A, B is a greater than C , A greater than C";
 	
-elif [[ "$NUM_A" > "$NUM_B" ]] && [[ "$NUM_A" > "$NUM_C" ]] && [[ "$NUM_B" < "$NUM_C" ]]; then
+elif [[ "$NUM_A" -gt "$NUM_B" ]] && [[ "$NUM_A" -gt "$NUM_C" ]] && [[ "$NUM_B" -lt "$NUM_C" ]]; then
         echo "A is greater than B , A is a greater than C, B less than C";
 
-elif [[ "$NUM_A" > "$NUM_B" ]] && [[ "$NUM_A" > "$NUM_C" ]] && [[ "$NUM_B" > "$NUM_C" ]]; then
+elif [[ "$NUM_A" -gt "$NUM_B" ]] && [[ "$NUM_A" -gt "$NUM_C" ]] && [[ "$NUM_B" -gt "$NUM_C" ]]; then
         echo "A is greater than B , A is a greater than C, B greater than C";
 
-elif  [[ "$NUM_C" > "$NUM_A" ]] && [[ "$NUM_C" > "$NUM_B" ]] && [[ "$NUM_A" < "$NUM_B" ]]; then
+elif  [[ "$NUM_C" -gt "$NUM_A" ]] && [[ "$NUM_C" -gt "$NUM_B" ]] && [[ "$NUM_A" -lt "$NUM_B" ]]; then
         echo "C is greater than B and A, A less than B";
 
-elif  [[ "$NUM_C" > "$NUM_A" ]] && [[ "$NUM_C" > "$NUM_B" ]] && [[ "$NUM_B" < "$NUM_A" ]]; then
+elif  [[ "$NUM_C" -gt "$NUM_A" ]] && [[ "$NUM_C" -gt "$NUM_B" ]] && [[ "$NUM_B" -lt "$NUM_A" ]]; then
         echo "C is greater than B and A, A greater than B";
 fi
