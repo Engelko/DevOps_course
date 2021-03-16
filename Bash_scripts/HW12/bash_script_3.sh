48641b3a81b0c207ccd8112681215a09725db09c
Sergey Ewdokimow <ewdokimowengel@gmail.com>
2021-03-16 10:08:36 +0000

    added equality of numbers

--- Bash_scripts/HW12/bash_script_3.sh
+++ Bash_scripts/HW12/bash_script_3.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
-	echo "Please enter a number A "
+        echo "Please enter a number A "
 read NUM_A
 
-	echo "Please enter a number B "
+        echo "Please enter a number B "
 read NUM_B
 
-	echo "Please enter a number C "
+        echo "Please enter a number C "
 read NUM_C
 
-if (( $NUM_A > $NUM_B )) && (( $NUM_A > $NUM_C)); then 
-	echo "A is greater than B and A is a greater than C";
+if (($NUM_A == $NUM_B)) && (($NUM_A == $NUM_C)); then
+        echo "A equal B equal C";
 
-elif (( $NUM_B > $NUM_A)) && (($NUM_B > $NUM_C)); then 
-	echo "B is greater than A and B is a greater than C";
+elif (($NUM_A == $NUM_C)) && (($NUM_A != $NUM_B)); then
+        echo "C equal A and not equal B"
 
-elif (( $NUM_A == $NUM_B)) && (($NUM_A == $NUM_C)); then 
-	echo "A equal B equal C";
+elif (($NUM_A == $NUM_B)) && (($NUM_B != $NUM_C)); then
+        echo "A equal B and not equal C"
 
+elif (($NUM_B == $NUM_C)) && (($NUM_A != $NUM_C)); then
+        echo "B equal C and not equal A"
+
+elif (($NUM_B > $NUM_A)) && (($NUM_B > $NUM_C)); then
+        echo "B is greater than A and B is a greater than C";
+
+elif (($NUM_A > $NUM_B)) && (($NUM_A > $NUM_C)); then
+        echo "A is greater than B and A is a greater than C";
 else
-	echo "C is greater than B and A";
+        echo "C is greater than B and A";
 fi
-
