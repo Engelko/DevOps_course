331a51097fc94cc195dad45ad1db34ac80667067
Sergey Ewdokimow <ewdokimowengel@gmail.com>
2021-03-17 17:09:40 +0000

    change equal compare

--- Bash_scripts/HW13/script_hw13.sh
+++ Bash_scripts/HW13/script_hw13.sh
@@ -15,16 +15,16 @@ NUM_A=${A,,}
 NUM_B=${B,,}
 NUM_C=${C,,}
 
-if [[ "$NUM_A" == "$NUM_B" ]] && [[ "$NUM_A" == "$NUM_C" ]]; then
+if [[ "$NUM_A" -eq "$NUM_B" ]] && [[ "$NUM_A" -eq "$NUM_C" ]]; then
         echo "A equal B equal C";
 
-elif [[ "$NUM_A" == "$NUM_C" ]] && [[ "$NUM_A" != "$NUM_B" ]]; then
+elif [[ "$NUM_A" -eq "$NUM_C" ]] && [[ "$NUM_A" -ne "$NUM_B" ]]; then
         echo "C equal A and not equal B";
 
-elif [[ "$NUM_A" == "$NUM_B" ]] && [[ "$NUM_B" != "$NUM_C" ]]; then
+elif [[ "$NUM_A" -eq "$NUM_B" ]] && [[ "$NUM_B" -ne "$NUM_C" ]]; then
         echo "A equal B and not equal C";
 
-elif [[ "$NUM_B" == "$NUM_C" ]] && [[ "$NUM_A" != "$NUM_C" ]]; then
+elif [[ "$NUM_B" -eq "$NUM_C" ]] && [[ "$NUM_A" -ne "$NUM_C" ]]; then
         echo "B equal C and not equal A";
 
 elif [[ "$NUM_B" -gt "$NUM_A" ]] && [[ "$NUM_B" -gt "$NUM_C" ]] && [[ "$NUM_A" -lt "$NUM_C" ]]; then
