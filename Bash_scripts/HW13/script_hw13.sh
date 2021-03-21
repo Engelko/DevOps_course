c3c042e171c2908e67641b9516f7823d4da85d38
Sergey Ewdokimow <ewdokimowengel@gmail.com>
2021-03-21 16:55:10 +0000

    change equal

--- Bash_scripts/HW13/script_hw13.sh
+++ Bash_scripts/HW13/script_hw13.sh
@@ -18,14 +18,23 @@ NUM_C=${C,,}
 if [[ "$NUM_A" -eq "$NUM_B" ]] && [[ "$NUM_A" -eq "$NUM_C" ]]; then
         echo "A equal B equal C";
 
-elif [[ "$NUM_A" -eq "$NUM_C" ]] && [[ "$NUM_A" -ne "$NUM_B" ]]; then
-        echo "C equal A and not equal B";
+elif [[ "$NUM_A" -eq "$NUM_C" ]] && [[ "$NUM_B" -gt "$NUM_C" ]]; then
+        echo "A equal C and B is a greater C";
 
-elif [[ "$NUM_A" -eq "$NUM_B" ]] && [[ "$NUM_B" -ne "$NUM_C" ]]; then
-        echo "A equal B and not equal C";
+elif [[ "$NUM_A" -eq "$NUM_C" ]] && [[ "$NUM_B" -lt "$NUM_C" ]]; then
+        echo "A equal C and B less than C";
 
-elif [[ "$NUM_B" -eq "$NUM_C" ]] && [[ "$NUM_A" -ne "$NUM_C" ]]; then
-        echo "B equal C and not equal A";
+elif [[ "$NUM_A" -eq "$NUM_B" ]] && [[ "$NUM_C" -gt "$NUM_B" ]]; then
+        echo "A equal B and C is a greater B";
+
+elif [[ "$NUM_A" -eq "$NUM_B" ]] && [[ "$NUM_C" -lt "$NUM_B" ]]; then
+        echo "A equal B and C less than B";
+
+elif [[ "$NUM_B" -eq "$NUM_C" ]] && [[ "$NUM_A" -gt "$NUM_C" ]]; then
+        echo "B equal C and A is a greater C";
+
+elif [[ "$NUM_B" -eq "$NUM_C" ]] && [[ "$NUM_A" -lt "$NUM_C" ]]; then
+        echo "B equal C and A less than C";
 
 elif [[ "$NUM_B" -gt "$NUM_A" ]] && [[ "$NUM_B" -gt "$NUM_C" ]] && [[ "$NUM_A" -lt "$NUM_C" ]]; then
         echo "B is greater than A, B is a greater than C , A less than C";
