93af70f031e2d61fd2c7e22abebc83c07a68e96a
Sergey Ewdokimow <ewdokimowengel@gmail.com>
2021-03-23 13:58:15 +0000

    Finish him

--- Bash_scripts/HW13/script_hw13.sh
+++ Bash_scripts/HW13/script_hw13.sh
@@ -49,33 +49,65 @@ declare -i three=3
 declare -i four=4
 declare -i five=5
 
+array=("one" "two" "three" "four" "five")
+
 read -p "Please enter a number A	" A       
 read -p "Please enter a number B	" B    
 read -p "Please enter a number C	" C
 
-
+re='^[0-9]+$'
 NUM_A=${A,,}
 NUM_B=${B,,}
 NUM_C=${C,,}
 
-array=("one" "two" "three" "four" "five")
 
-for i in "${array[@]}" do
-	if [[ "$i" -eq "$NUM_A" ]]; then
-		for i in "${array[@]}" do
-			if [[ "$i" -eq "$NUM_B" ]] ; then
-				for i in "${array[@]}" do
-					if [[ "$i" -eq "$NUM_C" ]] ; then
-						UP=1
-					fi
-				done
-			fi
-		done
-    fi
-done
-
-if [[ $UP -eq 1 ]] ; then
+function not_number {
+if [[ " ${array[*]} " == *" $NUM_A "* && " ${array[*]} " == *" $NUM_B "* && " ${array[*]} " == *" $NUM_C "* ]]; then
 	num_compare
 else
 	echo "enter a number from 1 to 5"
 fi
+}
+
+function not_num_A {
+if [[ " ${array[*]} " == *" $NUM_A "* ]] ; then
+	num_compare
+else
+	echo "A enter a number from 1 to 5"
+fi
+}
+
+function not_num_B {
+if [[ " ${array[*]} " == *" $NUM_B "* ]] ; then
+	num_compare
+else
+	echo "B enter a number from 1 to 5"
+fi
+}
+
+function not_num_C {
+if [[ " ${array[*]} " == *" $NUM_C "* ]] ; then
+	num_compare
+else
+	echo "C enter a number from 1 to 5"
+fi
+}
+
+if ! [[ "${NUM_A}" =~ $re || "${NUM_B}" =~ $re || "${NUM_C}" =~ $re ]] ; then
+	not_number
+elif [[ "${NUM_A}" =~ $re && "${NUM_B}" =~ $re && "${NUM_C}" =~ $re ]] ; then
+	num_compare
+elif ! [[ "${NUM_A}" =~ $re ]] ; then
+	not_num_A
+elif ! [[ "${NUM_B}" =~ $re ]] ; then
+	not_num_B
+elif ! [[ "${NUM_C}" =~ $re ]] ; then
+	not_num_C
+else 
+	echo "err"
+fi
+
+
+
+
+
