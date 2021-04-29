a6a7ee6e72694c4c5fe0caa7cfa835dccb0df812
Sergey Ewdokimow <ewdokimowengel@gmail.com>
2021-04-29 12:43:40 +0300

    rm hw21 directory

--- Bash_scripts/HW21/in_hw20/roles/hw202/files/sysdlogging.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-TIMESTAMP=`date +%S-%M-%H_%d-%m-%Y`
-( echo '======= DATE' $TIMESTAMP '========' \
-&& echo '====TIME ZONE====' && timedatectl | grep "Time zone" \
-&& echo '====IP ADDRESS====' && hostname -I ) >> /tmp/custom_daemon.log
